9a8d5241d1320c403fc601bfc320b3b73b466018
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-21 17:24:21 +0300

    ADd gitignore

---
+++ veniq/__init__.py
@@ -0,0 +1,7 @@
+"""
+Veniq uses Machine Learning to analyze source code, find possible refactorings, and suggest those that seem optimal
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Yegor Bugayenko'
+__licence__ = 'MIT'
