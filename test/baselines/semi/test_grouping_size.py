c46a6bb9bbba4a9c9742479d36422c6195db65f2
Katya Garmash <katya@LAPTOP-6LP5LUHF.china.huawei.com>
2020-09-22 11:36:33 +0300

    split test

--- test/baselines/semi/test_grouping_size.py
+++ test/baselines/semi/test_grouping_size.py
@@ -10,6 +10,9 @@ class SimilarSizeTest(TestCase):
         example_1_2 = (2, 34)
         self.assertEqual(is_similar_size(example_1_1, example_1_2), True)
 
+    def test_lower_threshold(self):
+        example_1_1 = (2, 32)
+        example_1_2 = (2, 34)
         max_diff = 0.05
         self.assertEqual(is_similar_size(example_1_1, example_1_2, max_size_difference=max_diff),
                          False)
