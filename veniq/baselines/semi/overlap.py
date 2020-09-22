26059a6a41e4b6417da2f7588315550711172cd3
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-22 10:56:06 +0300

    Fix comments

--- veniq/baselines/semi/overlap.py
+++ veniq/baselines/semi/overlap.py
@@ -1,35 +1,7 @@
-
-# The MIT License (MIT)
-#
-# Copyright (c) 2020 Veniq
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-from itertools import combinations
 from typing import List
-import operator
-
-MIN_OVERLAP = 0.1
 
 
-def is_overlap(a: List[int], b: List[int]):
+def is_overlap(a: List[int], b: List[int], min_overlap=0.1):
     """
     Checks whether 2 opportunities are overlapping
 
@@ -37,7 +9,7 @@ def is_overlap(a: List[int], b: List[int]):
     the second is end statements number
     :param b: array of 2 elements, the first is start statements number,
     the second is end statements number
-
+    :param min_overlap: min accepted value
     :return: True if it is an overlap, False if it is not
     """
 
@@ -45,7 +17,4 @@ def is_overlap(a: List[int], b: List[int]):
     b_LoC = abs(b[1] - b[0])
     overlap = len(set(range(a[0], a[1] + 1)) & set(range(b[0], b[1] + 1)))
     overlap_index = overlap / float(max(a_LoC, b_LoC))
-    if overlap_index >= MIN_OVERLAP:
-        return True
-    else:
-        return False
+    return overlap_index >= min_overlap
