b4a2ba145e55a0d2edce0c63c5d04aeee2de71f8
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-22 11:52:26 +0300

    Add setup.py

---
+++ veniq/__init__.py
@@ -0,0 +1,8 @@
+"""
+Veniq uses Machine Learning to analyze source code,
+find possible refactorings, and suggest those that seem optimal
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Anton Cheshkov, Vitaly Protasov, Ekaterina Garmash'
+__licence__ = 'MIT'
