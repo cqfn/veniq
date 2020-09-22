0ebbf239493dd968673b8aca52dd98c8426d9ac5
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-22 11:42:48 +0300

    Fix flake8

--- veniq/__init__.py
+++ veniq/__init__.py
@@ -1,5 +1,6 @@
 """
-Veniq uses Machine Learning to analyze source code, find possible refactorings, and suggest those that seem optimal
+Veniq uses Machine Learning to analyze source code,
+find possible refactorings, and suggest those that seem optimal
 """
 
 __version__ = '0.0.0'
