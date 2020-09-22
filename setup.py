c08ddb184bec1466700c7f1c23d1b21680e2f794
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-22 11:56:22 +0300

    Fix

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2020 Veniq
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
 from setuptools import setup, find_packages
 import veniq
 
@@ -28,7 +5,8 @@ setup(
     name='veniq',
     version=veniq.__version__,
     description=veniq.__doc__.strip(),
-    long_description='Veniq uses Machine Learning to analyze source code, find possible refactorings, and suggest those that seem optimal',
+    long_description='Veniq uses Machine Learning to analyze source code, '
+                     'find possible refactorings, and suggest those that seem optimal',
     url='https://github.com/cqfn/veniq.git',
     download_url='https://github.com/cqfn/veniq.git',
     author=veniq.__author__,
