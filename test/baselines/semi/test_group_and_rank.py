5622a8eaede58eb85b089f21947cf7a67ba01990
Katya Garmash <katya@LAPTOP-6LP5LUHF.china.huawei.com>
2020-09-24 17:43:29 +0300

    merge benefit f-n code and group&rank code; no tests yet

--- test/baselines/semi/test_group_and_rank.py
+++ test/baselines/semi/test_group_and_rank.py
@@ -13,8 +13,31 @@ class GroupAndRankTest(TestCase):
     oport_3 = (33, 50)
     oport_4 = (35, 50)
     oport_5 = (55, 65)
-    oportunities = [oport_0, oport_1, oport_2,
-                    oport_3, oport_4, oport_5]
+    line_to_semantic_dict = {
+        3: ['length', 'rcs'],
+        4: ['length', 'i', 'rcs'],
+        5: [],
+        6: ['rcs', 'i'],
+        7: ['rcs', 'rec', 'i', 'grabRes'],
+        9: ['rcs', 'rec', 'i', 'grabNonFileSetRes'],
+        11: ['length', 'rec', 'j'],
+        12: ['rec', 'j', 'getName', 'replace'],
+        13: ['rcs', 'i'],
+        14: ['rcs', 'i'],
+        15: ['afs', 'equals', 'getFullpath', 'getProj'],
+        16: ['name.afs', 'getProj', 'getFullpath'],
+        17: ['afs', 'equals', 'getProj', 'getPref'],
+        18: ['afs', 'getPref', 'getProj'],
+        19: ['pr', 'endsWith'],
+        20: ['pr'],
+        22: ['name', 'pr'],
+        25: ['MANIFEST_NAME', 'name', 'equalsIgnoreCase'],
+        26: ['j', 'manifests', 'rec', 'i'],
+        30: ['manifests', 'i'],
+        31: ['manifests', 'i'],
+        34: ['manifests']
+    }
+
 
     def test_in_same_group_1(self):
         self.assertTrue(in_same_group(self.oport_1, self.oport_2))
@@ -36,36 +59,36 @@ class GroupAndRankTest(TestCase):
         self.assertFalse(in_same_group(self.oport_3, self.oport_4,
                                        min_overlap=0.9))
 
-    def test_group_and_rank_in_groups(self):
-        expect_primary = [self.oport_0, self.oport_1,
-                          self.oport_3, self.oport_5]
+    # def test_group_and_rank_in_groups(self):
+    #     expect_primary = [self.oport_0, self.oport_1,
+    #                       self.oport_3, self.oport_5]
 
-        self.assertEqual(set(group_and_rank_in_groups(self.oportunities)),
-                         set(expect_primary))
+    #     self.assertEqual(set(group_and_rank_in_groups(self.oportunities)),
+    #                      set(expect_primary))
 
-    def test_group_and_rank_in_groups_custom_overlap(self):
-        expect_primary = [self.oport_0, self.oport_1,
-                          self.oport_3, self.oport_4, self.oport_5]
+    # def test_group_and_rank_in_groups_custom_overlap(self):
+    #     expect_primary = [self.oport_0, self.oport_1,
+    #                       self.oport_3, self.oport_4, self.oport_5]
 
-        self.assertEqual(set(group_and_rank_in_groups(self.oportunities,
-                                                      min_overlap=0.9)),
-                         set(expect_primary))
+    #     self.assertEqual(set(group_and_rank_in_groups(self.oportunities,
+    #                                                   min_overlap=0.9)),
+    #                      set(expect_primary))
 
-    def test_output_best_oportunities(self):
-        expect_list = [self.oport_0, self.oport_1,
-                       self.oport_3, self.oport_5]
-        self.assertEqual(output_best_oportunities(self.oportunities),
-                         expect_list)
+    # def test_output_best_oportunities(self):
+    #     expect_list = [self.oport_0, self.oport_1,
+    #                    self.oport_3, self.oport_5]
+    #     self.assertEqual(output_best_oportunities(self.oportunities),
+    #                      expect_list)
 
-    def test_output_best_oportunities_top1(self):
-        expect_list = [self.oport_0]
-        self.assertEqual(output_best_oportunities(self.oportunities, top_k=1),
-                         expect_list)
+    # def test_output_best_oportunities_top1(self):
+    #     expect_list = [self.oport_0]
+    #     self.assertEqual(output_best_oportunities(self.oportunities, top_k=1),
+    #                      expect_list)
 
-    def test_output_best_oportunities_custom_overlap_top4(self):
-        expect_list = [self.oport_0, self.oport_1,
-                       self.oport_3, self.oport_4]
-        self.assertEqual(output_best_oportunities(self.oportunities,
-                                                  top_k=4,
-                                                  min_overlap=0.9),
-                         expect_list)
+    # def test_output_best_oportunities_custom_overlap_top4(self):
+    #     expect_list = [self.oport_0, self.oport_1,
+    #                    self.oport_3, self.oport_4]
+    #     self.assertEqual(output_best_oportunities(self.oportunities,
+    #                                               top_k=4,
+    #                                               min_overlap=0.9),
+    #                      expect_list)
--- veniq/baselines/semi/benefits.py
+++ veniq/baselines/semi/benefits.py
@@ -1,15 +1,12 @@
 from collections import Counter
 from typing import Tuple, Dict, List
-from aibolit.ast_framework import AST, ASTNodeType, ASTNode
-from aibolit.utils.ast_builder import build_ast
-from aibolit.extract_method_baseline.extract_semantic import extract_method_statements_semantic, StatementSemantic
 
 
 def _check_is_common(
-    dict_file: Dict[int, List[str]],
-    statement_1: int,
-    statement_2: int
-) -> bool:
+        dict_file: Dict[int, List[str]],
+        statement_1: int,
+        statement_2: int
+        ) -> bool:
     '''
     This function is aimed to check whether 2 statements have
     common semantics or not.
@@ -19,63 +16,36 @@ def _check_is_common(
     return len(list(duplicates)) >= 1
 
 
-def _reprocess_dict(method_semantic: Dict[ASTNode, StatementSemantic]) -> Dict[int, List[str]]:
-    reprocessed_dict = dict()
-    for statement in method_semantic.keys():
-        new_values = []
-        new_values += list(method_semantic[statement].used_variables)
-        new_values += list(method_semantic[statement].used_objects)
-        new_values += list(method_semantic[statement].used_methods)
-        reprocessed_dict[statement.line] = new_values
-    return reprocessed_dict
-
-
-def _get_dict(filepath: str) -> Dict[int, List[str]]:
-    ast = AST.build_from_javalang(build_ast(filepath))
-    classes_declarations = (
-        node for node in ast.get_root().types
-        if node.node_type == ASTNodeType.CLASS_DECLARATION
-    )
-
-    methods_declarations = (
-        method_declaration for class_declaration in classes_declarations
-        for method_declaration in class_declaration.methods
-    )
-
-    methods_ast_and_class_name = (
-        (ast.get_subtree(method_declaration), method_declaration.parent.name)
-        for method_declaration in methods_declarations
-    )
-
-    for method_ast, _ in methods_ast_and_class_name:
-        # method_name = method_ast.get_root().name
-        original_method_semantic = extract_method_statements_semantic(method_ast)
-    reprocessed_dict = _reprocess_dict(original_method_semantic)
-    return reprocessed_dict
-
-
-def _LCOM2(file_dict: Dict, range_stats=[], mode='original') -> int:
+def _LCOM2(line_to_semantic_dict: Dict,
+           range_statements: Tuple[int, int] = None,
+           mode='original') -> int:
     '''
     LCOM_2 = P - Q;
     P is the number of pairs of statements
     that do not share variables and Q is the number
-    of pairs of lines that share variables
+    of pairs of lines that share variables.
+
+    line_to_semantic_dict is a map from line number
+    to variables that are referenced there.
+    range_statements is the range of lines for which LCOM2
+    is calculated.
     '''
     P = 0
     Q = 0
     list_statements = []
 
     if mode == 'after_ref':
-        list_statements = [i for i in file_dict if i < range_stats[0] or i > range_stats[1]]
+        list_statements = list(range(range_statements[0])) + list(range(range_statements[1]+1,
+                                                                        len(line_to_semantic_dict)))
     elif mode == 'opportunity':
-        list_statements = [i for i in range(range_stats[0], range_stats[1] + 1)]
+        list_statements = list(range(range_statements[0], range_statements[1] + 1))
     else:
-        list_statements = list(file_dict.keys())
+        list_statements = list(line_to_semantic_dict.keys())
 
     for stat_1 in list_statements:
         for stat_2 in list_statements:
             if stat_1 < stat_2:
-                if _check_is_common(file_dict, stat_1, stat_2):
+                if _check_is_common(line_to_semantic_dict, stat_1, stat_2):
                     Q += 1
                 else:
                     P += 1
@@ -90,17 +60,17 @@ def _get_benefit(dict_semantic: Dict[int, List[str]], range_stats: Tuple[int, in
 
 
 def is_first_more_benefit(
-    path_original_code: str,
-    range_1: Tuple[int, int],
-    range_2: Tuple[int, int],
-    difference_threshold: float = 0.01
-) -> bool:
+        line_to_semantic_dict: Dict[int, List[str]],
+        range_1: Tuple[int, int],
+        range_2: Tuple[int, int],
+        difference_threshold: float = 0.01
+    ) -> bool:
     """
     Takes two opportunities and check if first opportunity
     is more benefit than the second one.
     """
-    dict_semantic = _get_dict(path_original_code)
-    first_benefit = _get_benefit(dict_semantic, range_1)
-    second_benefit = _get_benefit(dict_semantic, range_2)
-    diff_between_benefits = abs(first_benefit - second_benefit) / max(first_benefit, second_benefit)
+    first_benefit = _get_benefit(line_to_semantic_dict, range_1)
+    second_benefit = _get_benefit(line_to_semantic_dict, range_2)
+    diff_between_benefits = abs(first_benefit - second_benefit) / \
+                             max(first_benefit, second_benefit)
     return diff_between_benefits >= difference_threshold
