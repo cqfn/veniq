df49f87e5f383d47e2e7eeaca2d6df742414a4e8
Vitaly-Protasov <izakladno@yandex.ru>
2020-09-24 09:56:21 +0300

    added docstring

--- veniq/baselines/semi/benefits.py
+++ veniq/baselines/semi/benefits.py
@@ -10,10 +10,10 @@ def _check_is_common(
     statement_1: int,
     statement_2: int
 ) -> bool:
-'''
-This function is aimed to check whether 2 statements have 
-common semantics or not.
-'''
+    '''
+    This function is aimed to check whether 2 statements have 
+    common semantics or not.
+    '''
     joined_names: Counter = Counter(dict_file[statement_1] + dict_file[statement_2])
     duplicates = {element: count for element, count in joined_names.items() if count > 1}.keys()
     return len(list(duplicates)) >= 1
