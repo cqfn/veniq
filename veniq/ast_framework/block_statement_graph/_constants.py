c5442130c392e2553e09f63ce30813e4c19d78b2
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-09-28 21:23:20 +0300

    [23] Add functionality to detect node types.

--- veniq/ast_framework/block_statement_graph/_constants.py
+++ veniq/ast_framework/block_statement_graph/_constants.py
@@ -1,14 +1,26 @@
 from enum import Enum, auto
 
-
-class NodeType(Enum):
-    Statement = auto()
-    Block = auto()
+from networkx import DiGraph
 
 
 # networkx field names
 NODE = "node"
 BLOCK_REASON = "block_reason"
+
+
+class NodeType(Enum):
+    Statement = auto()
+    Block = auto()
+
+    @staticmethod
+    def get_node_type(graph: DiGraph, node_id: int) -> "NodeType":
+        node_attributes = graph.nodes(data=True)[node_id]
+        if NODE in node_attributes:
+            return NodeType.Statement
+        elif BLOCK_REASON in node_attributes:
+            return NodeType.Block
+        else:
+            raise ValueError(f"Cannot identify node with attributes {node_attributes}")
 
 
 class BlockReason(Enum):
