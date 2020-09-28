5394bf61b72f02e0e39d7405063fd5958167ea1a
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-09-28 13:15:26 +0300

    [23] Add extractors for single block statements.

--- veniq/ast_framework/block_statement_graph/_block_extractors.py
+++ veniq/ast_framework/block_statement_graph/_block_extractors.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List, NamedTuple
+from typing import Callable, Dict, List, NamedTuple, Union
 
 from veniq.ast_framework import ASTNode, ASTNodeType
 from ._constants import BlockReason
@@ -20,11 +20,42 @@ def _extract_blocks_from_plain_statement(statement: ASTNode) -> List[BlockInfo]:
     return []
 
 
+def _extract_blocks_from_single_block_statement_factory(
+    field_name: str,
+) -> Callable[[ASTNode], List[BlockInfo]]:
+    def extract_blocks_from_single_block_statement(statement: ASTNode) -> List[BlockInfo]:
+        return [
+            BlockInfo(
+                reason=BlockReason.SINGLE_BLOCK,
+                statements=_unwrap_block_to_statements_list(getattr(statement, field_name)),
+            )
+        ]
+
+    return extract_blocks_from_single_block_statement
+
+
+def _unwrap_block_to_statements_list(
+    block_statement_or_statement_list: Union[ASTNode, List[ASTNode]]
+) -> List[ASTNode]:
+    if isinstance(block_statement_or_statement_list, ASTNode):
+        assert block_statement_or_statement_list.node_type == ASTNodeType.BLOCK_STATEMENT
+        return block_statement_or_statement_list.statements
+
+    return block_statement_or_statement_list
+
+
 _block_extractors: Dict[ASTNodeType, Callable[[ASTNode], List[BlockInfo]]] = {
+    # plain statements
     ASTNodeType.ASSERT_STATEMENT: _extract_blocks_from_plain_statement,
     ASTNodeType.BREAK_STATEMENT: _extract_blocks_from_plain_statement,
     ASTNodeType.CONTINUE_STATEMENT: _extract_blocks_from_plain_statement,
     ASTNodeType.RETURN_STATEMENT: _extract_blocks_from_plain_statement,
     ASTNodeType.STATEMENT_EXPRESSION: _extract_blocks_from_plain_statement,
     ASTNodeType.THROW_STATEMENT: _extract_blocks_from_plain_statement,
+    # single block statements
+    ASTNodeType.BLOCK_STATEMENT: _extract_blocks_from_single_block_statement_factory("body"),
+    ASTNodeType.DO_STATEMENT: _extract_blocks_from_single_block_statement_factory("statements"),
+    ASTNodeType.FOR_STATEMENT: _extract_blocks_from_single_block_statement_factory("body"),
+    ASTNodeType.SYNCHRONIZED_STATEMENT: _extract_blocks_from_single_block_statement_factory("block"),
+    ASTNodeType.WHILE_STATEMENT: _extract_blocks_from_single_block_statement_factory("body"),
 }
