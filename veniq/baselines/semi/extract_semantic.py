928d3a14cdf5fbf5743d49ac6a3b3db8d1eb8213
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-10-01 20:44:24 +0300

    [29] Add similarity method to statements semantic class.

--- veniq/baselines/semi/extract_semantic.py
+++ veniq/baselines/semi/extract_semantic.py
@@ -12,6 +12,11 @@ class StatementSemantic:
     used_objects: Set[str] = field(default_factory=set)
     used_methods: Set[str] = field(default_factory=set)
 
+    def is_similar(self, other: "StatementSemantic") -> bool:
+        return len(self.used_variables & other.used_variables) != 0 or \
+            len(self.used_objects & other.used_objects) != 0 or \
+            len(self.used_methods & other.used_methods) != 0
+
 
 def extract_method_statements_semantic(method_ast: AST) -> Dict[ASTNode, StatementSemantic]:
     statement_semantic: Dict[ASTNode, StatementSemantic] = OrderedDict()
