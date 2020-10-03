c55747ede46e3f2c49b79719f2d9873b2c010f8e
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-10-03 11:26:44 +0300

    [31] Add CLI to ranking.

--- veniq/baselines/semi/rank_extraction_opportunities.py
+++ veniq/baselines/semi/rank_extraction_opportunities.py
@@ -1,7 +1,11 @@
 from typing import List, Dict, Tuple, Iterator, NamedTuple
 
-from ._lcom2 import LCOM2
+from veniq.ast_framework import AST
+from .extract_semantic import extract_method_statements_semantic
+from .cluster_statements import cluster_statements
 from ._common_types import Statement, StatementSemantic, ExtractionOpportunity, OpportunityBenifit
+from ._common_cli import common_cli
+from ._lcom2 import LCOM2
 
 
 class ExtractionOpportunityGroupSettings(NamedTuple):
@@ -126,3 +130,28 @@ def _create_extraction_opportunities_group(
             extraction_opportunity_group.add_extraction_opportunity(extraction_opportunity)
 
     return extraction_opportunity_group
+
+
+def _print_extraction_opportunities(
+    method_ast: AST, filepath: str, class_name: str, method_name: str
+) -> None:
+    statements_semantic = extract_method_statements_semantic(method_ast)
+    extraction_opportunities = cluster_statements(statements_semantic)
+    extraction_opportunities_groups = rank_extraction_opportunities(
+        statements_semantic, extraction_opportunities
+    )
+
+    print(
+        f"Extraction opportunities groups of method {method_name} in class {class_name} in file {filepath}:"
+    )
+
+    for extraction_opportunity_group in extraction_opportunities_groups:
+        print(f"\tExtraction opportunities group with scope {extraction_opportunity_group.benifit}:")
+        for extraction_opportunity, benifit in extraction_opportunity_group.opportunities:
+            print(f"\t\tExtraction opportunity with score {benifit}:")
+            for statement in extraction_opportunity:
+                print(f"\t\t\t{statement.node_type} on line {statement.line}")
+
+
+if __name__ == "__main__":
+    common_cli(_print_extraction_opportunities, "Find extraction opportunities and rank them.")
