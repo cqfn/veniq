e69cb05cab97cefb78d6f34e06cd9946678ec74d
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-10-06 17:38:08 +0300

    [38] Move node type detection to node factory.

--- veniq/ast_framework/block_statement_graph/_constants.py
+++ veniq/ast_framework/block_statement_graph/_constants.py
@@ -1,7 +1,5 @@
 from enum import Enum
 
-from networkx import DiGraph
-
 NodeId = int
 
 # networkx field names
@@ -12,16 +10,6 @@ BLOCK_REASON = "block_reason"
 class NodeType(Enum):
     Statement = "Statement"
     Block = "Block"
-
-    @staticmethod
-    def detect(graph: DiGraph, node_id: NodeId) -> "NodeType":
-        node_attributes = graph.nodes(data=True)[node_id]
-        if NODE in node_attributes:
-            return NodeType.Statement
-        elif BLOCK_REASON in node_attributes:
-            return NodeType.Block
-        else:
-            raise ValueError(f"Cannot identify node with attributes {node_attributes}")
 
 
 class BlockReason(Enum):
