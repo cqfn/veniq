c1a9135a8844b37a7da150f110b93e0ac54d202b
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2020-10-07 14:55:11 +0300

    [42] Use 'used_objects_unwrapped' to check similarity.

--- veniq/baselines/semi/_common_types.py
+++ veniq/baselines/semi/_common_types.py
@@ -13,11 +13,11 @@ class StatementSemantic:
     used_methods: Set[str] = field(default_factory=set)
 
     def is_similar(self, other: "StatementSemantic") -> bool:
-        return len(self.used_objects & other.used_objects) != 0 or \
+        return len(self.used_objects_unwrapped & other.used_objects_unwrapped) != 0 or \
             len(self.used_methods & other.used_methods) != 0
 
     @property
-    def _used_objects_unwrap(self) -> Set[str]:
+    def used_objects_unwrapped(self) -> Set[str]:
         """
         Turns each name from "a.b.c" to "a", "a.b", "a.b.c"
         """
