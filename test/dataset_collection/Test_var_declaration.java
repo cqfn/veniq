5ec7664d7da09bac346a042293daa1c1c9f26295
Vitaly Protasov <izakladno@yandex.ru>
2020-10-20 16:39:26 +0300

    added several tests

---
+++ test/dataset_collection/Test_var_declaration.java
@@ -0,0 +1,42 @@
+public abstract class Test {
+	private static final EntityResolver NO_OP_ENTITY_RESOLVER =
+			(publicId, systemId) -> new InputSource(new StringReader(""));
+
+	/** Logger available to subclasses. */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	private boolean supportDtd = false;
+
+	private boolean processExternalEntities = false;
+
+	@Nullable
+	private DocumentBuilderFactory documentBuilderFactory;
+
+	private final Object documentBuilderFactoryMonitor = new Object();
+	
+	protected Document buildDocument() {
+		try {
+			DocumentBuilder documentBuilder;
+			synchronized (this.documentBuilderFactoryMonitor) {
+				if (this.documentBuilderFactory == null) {
+					this.documentBuilderFactory = createDocumentBuilderFactory();
+				}
+				documentBuilder = createDocumentBuilder(this.documentBuilderFactory);
+			}
+			return documentBuilder.newDocument();
+		}
+		catch (ParserConfigurationException ex) {
+			throw new UnmarshallingFailureException("Could not create document placeholder: " + ex.getMessage(), ex);
+		}
+	}
+
+	protected DocumentBuilderFactory createDocumentBuilderFactory() throws ParserConfigurationException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setValidating(false);
+		factory.setNamespaceAware(true);
+		factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", !isSupportDtd());
+		factory.setFeature("http://xml.org/sax/features/external-general-entities", isProcessExternalEntities());
+		return factory;
+	}
+	
+}
--- test/dataset_collection/test_dataset_collection.py
+++ test/dataset_collection/test_dataset_collection.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 from unittest import TestCase
+import os
 
 from veniq.dataset_collection.augmentation import (
     determine_algorithm_insertion_type,
@@ -8,7 +9,12 @@ from veniq.dataset_collection.augmentation import (
     is_match_to_the_conditions)
 from veniq.ast_framework import AST, ASTNodeType
 from veniq.dataset_collection.types_identifier import (
-    InlineTypesAlgorithms, InlineWithoutReturnWithoutArguments, InlineWithReturnWithoutArguments)
+    InlineTypesAlgorithms,
+    InlineWithoutReturnWithoutArguments,
+    InlineWithReturnWithoutArguments,
+    AlgorithmFactory)
+
+from veniq.utils.encoding_detector import read_text_with_autodetected_encoding
 from veniq.utils.ast_builder import build_ast
 
 
@@ -277,3 +283,37 @@ class TestDatasetCollection(TestCase):
             if x.member == 'severalReturns'][0]
         is_matched = is_match_to_the_conditions(ast, m_inv, m_decl_original)
         self.assertEqual(is_matched, False)
+
+    def test_inline_invocation_inside_var_declaration(self):
+        filepath = self.current_directory / "Test_var_declaration.java"
+        new_full_filename = self.current_directory / 'temp.java'
+        ast = AST.build_from_javalang(build_ast(filepath))
+        algorithm_type = InlineTypesAlgorithms.WITH_RETURN_WITHOUT_ARGUMENTS
+        algorithm_for_inlining = AlgorithmFactory().create_obj(algorithm_type)
+        algorithm_for_inlining().inline_function(filepath, 22, 34, 40, new_full_filename)
+        good_inlined_text = [
+        '            \t\tDocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();\r',
+        '            \t\tfactory.setValidating(false);\r',
+        '            \t\tfactory.setNamespaceAware(true);\r',
+        '            \t\tfactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", !isSupportDtd());\r',
+        '            \t\tfactory.setFeature("http://xml.org/sax/features/external-general-entities", isProcessExternalEntities());\r',
+        '                    this.documentBuilderFactory = factory;\r'
+        ]
+        text_lines = read_text_with_autodetected_encoding(str(new_full_filename)).split('\n')[21:27]
+        self.assertEqual(text_lines, good_inlined_text)
+        new_full_filename.unlink()
+
+    def test_inline_inside_invokation_several_lines(self):
+        filepath = self.current_directory / "Test_after_comment_inline.java"
+        new_full_filename = self.current_directory / 'temp.java'
+        ast = AST.build_from_javalang(build_ast(filepath))
+        algorithm_type = InlineTypesAlgorithms.WITH_RETURN_WITHOUT_ARGUMENTS
+        algorithm_for_inlining = AlgorithmFactory().create_obj(algorithm_type)
+        algorithm_for_inlining().inline_function(filepath, 14, 18, 20, new_full_filename)
+        good_inlined_text = [
+        '\t\t\tgetModel().fireComponentChangedEvent(new ComponentEvent(this,\r',
+        '\t\t\t\t\tComponentEvent.EventType.CHILD_REMOVED));\r'
+        ]
+        text_lines = read_text_with_autodetected_encoding(str(new_full_filename)).split('\n')[13:15]
+        self.assertEqual(text_lines, good_inlined_text)
+        new_full_filename.unlink()
