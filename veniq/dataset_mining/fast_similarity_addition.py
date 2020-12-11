b576838232d9a47374ce33c37bff9e90dd29be3c
Evgeny Maslov <lyriccoder@gmail.com>
2020-12-11 16:18:37 +0300

    ADd multicore

--- veniq/dataset_mining/fast_similarity_addition.py
+++ veniq/dataset_mining/fast_similarity_addition.py
@@ -1,32 +1,31 @@
-from dataclasses import dataclass
-from typing import Optional, Tuple
+import os
+import traceback
+from argparse import ArgumentParser
+from ast import literal_eval
+from pathlib import Path
 
 import pandas as pd
+from pebble import ProcessPool
+from tqdm import tqdm
 
-from veniq.utils.encoding_detector import read_text_with_autodetected_encoding
+from veniq.ast_framework import AST
 from veniq.ast_framework import ASTNodeType
 from veniq.dataset_collection.augmentation import method_body_lines
-from veniq.utils.ast_builder import build_ast
-from veniq.ast_framework import AST
-from pathlib import Path
-from ast import literal_eval
 from veniq.dataset_mining.code_similarity import is_similar_functions, get_after_lines
+from veniq.utils.ast_builder import build_ast
 
-current_directory = Path(r'D:\temp\dataset_colelction_refactoring\12_07')
 
-df = pd.read_csv('temp_filtered.csv')
-df['real_extractions'] = df['real_extractions'].apply(literal_eval)
-for index, row in df.iterrows():
+def calculate_additional_similarity(row: pd.Series):
     target_function_name = row['function_name_with_LM']
-    real_extractions = row['real_extractions']
+    # real_extractions = row['real_extractions']
     class_name = row['class_name']
-    filepath_after = str(current_directory / row['filepath_saved'])
+    filepath_after = str(csv_output / row['filepath_saved'])
     filepath_before = filepath_after.replace('_after.java', '_before.java')
-    extracted_function_name = row['function_inlined']
+    # extracted_function_name = row['function_inlined']
     function_target_start_line = row['function_target_start_line']
     function_target_end_line = row['function_target_end_line']
     ast_after = AST.build_from_javalang(build_ast(filepath_after))
-    ast_before = AST.build_from_javalang(build_ast(filepath_before))
+    # ast_before = AST.build_from_javalang(build_ast(filepath_before))
     class_ = [x for x in ast_after.get_proxy_nodes(
         ASTNodeType.CLASS_DECLARATION,
         ASTNodeType.ENUM_DECLARATION,
@@ -39,8 +38,8 @@ for index, row in df.iterrows():
     )
 
     _, lines_matched, _, _, how_similar_btw_extractions_target_modified, _ = is_similar_functions(
-        str(current_directory / filepath_before),
-        str(current_directory / filepath_after),
+        str(csv_output / filepath_before),
+        str(csv_output / filepath_after),
         [[function_target_start_line + 1, function_target_end_line]],
         (body_start_line_mdfd, body_end_line_mdfd)
     )
@@ -54,5 +53,55 @@ for index, row in df.iterrows():
     # will differ every time
     lines_matched += 1
     target_modified_size = len(after_lines[0])
-    df['similarity_modified'] = float(lines_matched) / target_modified_size
-    df.to_csv('filtered_target_modified.csv')
+    row['similarity_modified'] = float(lines_matched) / target_modified_size
+    return row
+
+
+if __name__ == '__main__':  # noqa: C901
+    system_cores_qty = os.cpu_count() or 1
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-d", "--dir",
+        required=True,
+        help="File path to JAVA similarity dataset"
+    )
+    parser.add_argument(
+        "-i", "--input",
+        required=True,
+        help="Input filtered similarity csv dataset file"
+    )
+    parser.add_argument(
+        "-o", "--out",
+        required=True,
+        help="Output csv file",
+        default='filtered_target.csv'
+    )
+    parser.add_argument(
+        "--jobs",
+        "-j",
+        type=int,
+        default=system_cores_qty - 1,
+        help="Number of processes to spawn. "
+             "By default one less than number of cores. "
+             "Be careful to raise it above, machine may stop responding while creating dataset.",
+    )
+
+    args = parser.parse_args()
+    csv_output = Path(args.out, 'filtered_results_with_target.csv')
+
+    df = pd.read_csv('temp_filtered.csv')
+    df['real_extractions'] = df['real_extractions'].apply(literal_eval)
+
+    new_df = pd.DataFrame(columns=df.columns)
+    with ProcessPool(system_cores_qty) as executor:
+        future = executor.map(calculate_additional_similarity)
+        result = future.result()
+
+        for filename in tqdm(df.iterrows(), total=df.shape[0]):
+            try:
+                row = next(result)
+                new_df.append(row, ignore_index=True)
+            except Exception as e:
+                traceback.print_stack()
+
+    new_df.to_csv(args.out)
