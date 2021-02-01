f30c019bc50947f8f2fb47a882c286ed27645adc
Ekaterina Garmash WX5328958 <ekaterina.garmash@huawei.com>
2021-02-01 15:41:09 +0300

    more readable tests

--- test/baselines/semi/test_recommend.py
+++ test/baselines/semi/test_recommend.py
@@ -7,6 +7,7 @@ from veniq.ast_framework import AST
 from veniq.baselines.semi.recommend import _add_class_decl_wrap,\
     _convert_ExtractionOpportunity_to_EMO, _get_method_subtree,\
     recommend_for_method
+from test.baselines.semi.utils import create_extraction_opportunity
 
 
 class TestRecommend(TestCase):
@@ -51,63 +52,48 @@ class TestRecommend(TestCase):
                               "int y = x + 2;",
                               "return y; } }"]
 
-    def test_add_class_decl_wrap(self):
-        class_decl = _add_class_decl_wrap(self._method)
-        self.assertEqual(class_decl, self._class)
-
-    def test_get_method_subtree(self):
+    def _get_method_ast(self, class_decl):
         with NamedTemporaryFile(delete=False) as f:
             _name = f.name
-            f.write('\n'.join(self._class).encode())
+            f.write('\n'.join(class_decl).encode())
         ast = AST.build_from_javalang(build_ast(_name))
         os.unlink(_name)
 
         nodes = list(ast)
         _method_decl = ast.get_subtree(nodes[4])
+        return _method_decl
+
+    def test_add_class_decl_wrap(self):
+        class_decl = _add_class_decl_wrap(self._method)
+        self.assertEqual(class_decl, self._class)
+
+    def test_get_method_subtree(self):
+        _method_decl = self._get_method_ast(self._class)
         self.assertEqual(str(_method_decl),
                          str(_get_method_subtree(self._class)))
 
     def test_convert_ExtractionOpportunity_to_EMO_1(self):
-        with NamedTemporaryFile(delete=False) as f:
-            _name = f.name
-            f.write('\n'.join(self._class).encode())
-
-        ast = AST.build_from_javalang(build_ast(_name))
-        os.unlink(_name)
-        _ast = ast.get_subtree(list(ast)[4])
-        extr_opport_nodes = [list(_ast)[18], list(_ast)[10]]
-
-        object_ExtractionOpportunity = tuple(extr_opport_nodes)
+        _ast = self._get_method_ast(self._class)
+        object_ExtractionOpportunity, _ = create_extraction_opportunity(_ast,
+                                                                        [3, 4])
         expect_EMO = (2, 3)
         result_EMO = _convert_ExtractionOpportunity_to_EMO(
             object_ExtractionOpportunity, self._class)
         self.assertEqual(expect_EMO, result_EMO)
 
     def test_convert_ExtractionOpportunity_to_EMO_2(self):
-        with NamedTemporaryFile(delete=False) as f:
-            _name = f.name
-            f.write('\n'.join(self._class_2).encode())
-
-        ast = AST.build_from_javalang(build_ast(_name))
-        os.unlink(_name)
-
-        extr_opport_nodes = [list(ast)[18], list(ast)[26]]
-        object_ExtractionOpportunity = tuple(extr_opport_nodes)
+        _ast = self._get_method_ast(self._class_2)
+        object_ExtractionOpportunity, _ = create_extraction_opportunity(
+            _ast, [4, 5, 6])
         expect_EMO = (3, 5)
         result_EMO = _convert_ExtractionOpportunity_to_EMO(
             object_ExtractionOpportunity, self._class_2)
         self.assertEqual(expect_EMO, result_EMO)
 
     def test_EMO_no_open_bracket(self):
-        with NamedTemporaryFile(delete=False) as f:
-            _name = f.name
-            f.write('\n'.join(self._class_2_close_bracket).encode())
-
-        ast = AST.build_from_javalang(build_ast(_name))
-        os.unlink(_name)
-
-        extr_opport_nodes = [list(ast)[27]]
-        object_ExtractionOpportunity = tuple(extr_opport_nodes)
+        _ast = self._get_method_ast(self._class_2_close_bracket)
+        object_ExtractionOpportunity, _ = create_extraction_opportunity(_ast,
+                                                                        [5])
         expect_EMO = (4, 4)
         result_EMO = _convert_ExtractionOpportunity_to_EMO(
             object_ExtractionOpportunity, self._class_2_close_bracket)
@@ -121,14 +107,9 @@ class TestRecommend(TestCase):
         "return y; } }"
         Should return just these two lines.
         """
-        with NamedTemporaryFile(delete=False) as f:
-            _name = f.name
-            f.write('\n'.join(self._class_2_close_bracket).encode())
-
-        ast = AST.build_from_javalang(build_ast(_name))
-        os.unlink(_name)
-        extr_opport_nodes = [list(ast)[53], list(ast)[66]]
-        object_ExtractionOpportunity = tuple(extr_opport_nodes)
+        _ast = self._get_method_ast(self._class_2_close_bracket)
+        object_ExtractionOpportunity, _ = create_extraction_opportunity(_ast,
+                                                                        [7, 8])
         expect_EMO = (6, 7)
         result_EMO = _convert_ExtractionOpportunity_to_EMO(
             object_ExtractionOpportunity, self._class_2_close_bracket)
