1a4c9a69e4b14155dffa3a0feb9db9d544d5c147
Ekaterina Garmash WX5328958 <ekaterina.garmash@huawei.com>
2021-02-01 16:50:50 +0300

    exceptions and names

--- veniq/baselines/semi/recommend.py
+++ veniq/baselines/semi/recommend.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple
 from tempfile import NamedTemporaryFile
 from functools import reduce
 from operator import itemgetter
@@ -20,10 +20,10 @@ from veniq.baselines.semi._common_types import ExtractionOpportunity,\
     OpportunityBenifit
 
 
-EMO = Tuple[int, int]
+EMORange = Tuple[int, int]
 
 
-class WrongMethodFormatException(Exception):
+class WrongMethodFormat(ValueError):
     pass
 
 
@@ -82,7 +82,7 @@ def _find_closing_brackets(extraction_till_end: List[str], num_extr_lines_orig: 
 
 
 def _convert_ExtractionOpportunity_to_EMO(
-        extr_opport: ExtractionOpportunity, class_decl: List[str]) -> EMO:
+        extr_opport: ExtractionOpportunity, class_decl: List[str]) -> EMORange:
     ''' Converts extraction opportunity of type ExtractionOpportunity from
     veniq.baselines.semi._common_types to type EMO defined here.
     '''
@@ -109,38 +109,38 @@ def check_format_validity(method_decl: List[str]) -> None:
     pass
 
 
-def recommend_for_method(method_decl: List[str]) -> Union[List[EMO], str]:
+def recommend_for_method(method_decl: List[str]) -> List[EMORange]:
     '''
     Takes method declaration in form of list of lines,
-    outputs list of EMOs in the order of decreasing recommendation.
-    EMO is a (start_line_extraction, end_line_extraction)
+    outputs list of EMORanges in the order of decreasing recommendation.
+    EMORange is a (start_line_extraction, end_line_extraction)
     (the range is inclusive).
 
-    TODO: use error codes in the future insteach of string
+    # TODO: use error codes in the future insteach of string
     error messages
     '''
     try:
         check_format_validity(method_decl)
-    except WrongMethodFormatException:
-        return "Wrong format"
+    except WrongMethodFormat as e:
+        raise e
 
     class_decl_fake = _add_class_decl_wrap(method_decl)
     try:
         method_subtree = _get_method_subtree(class_decl_fake)
-    except JavaSyntaxError:
-        return "Javalang can't parse"
-    emo_groups = _find_EMO_groups(method_subtree)
-    if emo_groups is None or emo_groups == []:
+    except JavaSyntaxError as e:
+        raise e
+    emo_groups_semi = _find_EMO_groups(method_subtree)
+    if emo_groups_semi is None or emo_groups_semi == []:
         return []
 
-    all_opportunities: List[Tuple[ExtractionOpportunity, OpportunityBenifit]] = \
-        reduce(lambda x, y: x + list(y.opportunities), emo_groups, [])
+    all_opportunities_semi: List[Tuple[ExtractionOpportunity, OpportunityBenifit]] = \
+        reduce(lambda x, y: x + list(y.opportunities), emo_groups_semi, [])
 
-    all_opportunities_ranked = sorted(all_opportunities, key=itemgetter(1),
-                                      reverse=True)
-    ranked_EMOs = [_convert_ExtractionOpportunity_to_EMO(
-        x[0], class_decl_fake) for x in all_opportunities_ranked]
+    all_opportunities_semi_ranked = sorted(all_opportunities_semi, key=itemgetter(1),
+                                           reverse=True)
+    emo_ranges_ranked = [_convert_ExtractionOpportunity_to_EMO(
+        x[0], class_decl_fake) for x in all_opportunities_semi_ranked]
 
     # subtract 1 because we added fake class declaration line
-    ranked_EMOs = [(x[0] - 1, x[1] - 1) for x in ranked_EMOs]
-    return ranked_EMOs
+    emo_ranges_ranked = [(x[0] - 1, x[1] - 1) for x in emo_ranges_ranked]
+    return emo_ranges_ranked
